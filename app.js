b7845711af7a04f3714bf4a97745ea02cbaef763
CodecrafterGeetika <geetikaranisahu@gmail.com>
2025-10-01 10:49:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-
+const map = L.map('map').setView([12.97,77.59], 11); // change center to your city
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
